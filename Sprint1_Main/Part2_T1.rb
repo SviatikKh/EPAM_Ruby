2577eaaf2edb8ddabbebab7f8def9142d56b96a8
SviatikKh <48651894+SviatikKh@users.noreply.github.com>
2020-12-10 22:45:22 +0200

    Sprint 1 Main tasks

---
+++ Sprint1_Main/Part2_T1.rb
@@ -0,0 +1,12 @@
+=begin
+    
+Створити змінні text1='Hello' і text2='World'.
+За допомогою цих змінних і операції складання рядків вивести на екран фразу «Hello
+world».
+    
+=end
+
+text1='Hello'
+text2='World'
+
+puts text1 + ' ' + text2
